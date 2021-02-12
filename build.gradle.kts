0e1e3a0642e640b42b5daf5e64ca7b83a7000993
Janne Koschinski <janne@kuschku.de>
2021-02-12 10:04:15 +0100

    Release 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ plugins {
   id("org.jetbrains.dokka") version "1.4.20"
   id("maven-publish")
   id("jacoco")
+  id("com.vanniktech.maven.publish") version "0.13.0"
 }
 
 repositories {
@@ -69,64 +70,5 @@ tasks.withType<KotlinCompile> {
     freeCompilerArgs = listOf(
       "-Xopt-in=kotlin.ExperimentalUnsignedTypes"
     )
-  }
-}
-
-tasks {
-  val sourcesJar by registering(Jar::class) {
-    dependsOn(JavaPlugin.CLASSES_TASK_NAME)
-    archiveClassifier.set("sources")
-    from(sourceSets["main"].allSource)
-  }
-
-  val javadocJar by registering(Jar::class) {
-    dependsOn("dokkaJavadoc")
-    archiveClassifier.set("javadoc")
-    from(javadoc)
-  }
-
-  artifacts {
-    archives(sourcesJar)
-    archives(javadocJar)
-    archives(jar)
-  }
-}
-
-publishing {
-  publications {
-    register<MavenPublication>("mavenJava") {
-      from(components["java"])
-      artifact(tasks["sourcesJar"])
-      artifact(tasks["javadocJar"])
-
-      groupId = "de.justjanne"
-      artifactId = "kotlin-bitflags"
-      version = "0.1.0"
-
-      pom.withXml {
-        asNode().apply {
-          appendNode("description", "Simple Bitflags for Kotlin")
-          appendNode("name", rootProject.name)
-          appendNode("url", "https://git.kuschku.de/justJanne/kotlin-bitflags")
-          appendNode("licenses").appendNode("license").apply {
-            appendNode("name", "Mozilla Public License Version 2.0")
-            appendNode("url", "https://www.mozilla.org/en-US/MPL/2.0/")
-          }
-          appendNode("developers").appendNode("developer").apply {
-            appendNode("name", "Janne Mareike Koschinski")
-            appendNode("email", "support@kuschku.de")
-            appendNode("organizationUrl", "https://kuschku.de/")
-          }
-          appendNode("scm").apply {
-            appendNode("connection", "scm:git:https://git.kuschku.de/justJanne/kotlin-bitflags.git")
-            appendNode("developerConnection", "scm:git:ssh://git.kuschku.de:2222/justJanne/kotlin-bitflags.git")
-            appendNode("url", "https://git.kuschku.de/justJanne/kotlin-bitflags")
-          }
-          appendNode("properties").apply {
-            appendNode("project.build.sourceEncoding", "UTF-8")
-          }
-        }
-      }
-    }
   }
 }
