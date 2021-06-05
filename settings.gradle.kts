65316165db79f6076ddfbfe878bb93a7fe459ae0
Janne Mareike Koschinski <janne@kuschku.de>
2021-06-05 22:19:01 +0200

    Update dependencies, remove jcenter

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,10 @@
-rootProject.name = "kotlin-bitflags"
+/*
+ * Kotlin Bitflags
+ * Copyright (c) 2021 Janne Mareike Koschinski
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://mozilla.org/MPL/2.0/.
+ */
 
-pluginManagement {
-  repositories {
-    gradlePluginPortal()
-    jcenter()
-  }
-}
+rootProject.name = "kotlin-bitflags"
